11b6a9921a61f4b52b324fbd16fe5b388329a2eb
xuandung6905 <155149514+xuandung6905@users.noreply.github.com>
2024-12-01 22:40:29 +0700

    sửa xóa mã giảm giá

--- views/admin/coupon/createCoupons.php
+++ views/admin/coupon/createCoupons.php
@@ -151,7 +151,7 @@
                 <div class="col-12 text-center">
                     <script>document.write(new Date().getFullYear())</script> &copy; Dự án 1 <iconify-icon
                         icon="iconamoon:heart-duotone" class="fs-18 align-middle text-danger"></iconify-icon> <a
-                        href="https://1.envato.market/techzaa" class="fw-bold footer-text" target="_blank">Nhóm 6</a>
+                        href="https://1.envato.market/techzaa" class="fw-bold footer-text" target="_blank">Nhóm 5</a>
                 </div>
             </div>
         </div>
