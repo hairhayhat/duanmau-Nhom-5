d21099437aabedebbd34c0315e2aebbf7039a275
Phonghehe1 <phongtvph52541@gmail.com>
2024-12-03 21:31:16 +0700

    xog cart

--- controllers/client/CartController.php
+++ controllers/client/CartController.php
@@ -3,7 +3,7 @@ require_once '../models/client/Cart.php';
 
 class CartController extends Cart
 {
-<<<<<<< HEAD
+
     public function index(){
         $carts = $this->getAllCart();
         include '../views/client/cart/cart.php';
@@ -54,42 +54,6 @@ class CartController extends Cart
             exit();
         }
     }
-=======
-//     public function addtoCart(): void
-//     {
-//         if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_to_cart'])) {
-//             if(empty($_POST['variant_id'])) {
-//                 $_SESSION['error'] = 'Vui lòng chọn sản phẩm và số lượng';
-//                 header(header: 'Location: ' . $_SERVER['HTTP_REFERER']);
-//                 exit();
-//             }
-//             $checkCart = $this->checkCart();
-//             if($checkCart) {
-//                 $quantity = $checkCart['quantity'] + $_POST['quantity'];
-//                 $updateCart = $this->updateCart(
-//                 $_SESSION['user']['user_id'],
-//                    $_POST['product_id'],
-//                     $_POST['variant_id'],
-//                     $_POST['quantity']
-//                 );
-//                 $_SESSION['success'] = 'Cập nhật giỏ hàng thành công';
-//                 header(header: 'Location: ' . $_SERVER['HTTP_REFERER']);
-//                 exit();
-//         }else {
-//             $addtoCart = $this->addtoCart(
-//                 $_SESSION['user']['user_id'],
-//                 $_POST['product_id'],
-//                 $_POST['variant_id'],
-//                 $_POST['quantity']
-//             );
-//             $_SESSION['success'] = 'Thêm vào giỏ hàng thành công';
-//             header(header: 'Location: ' . $_SERVER['HTTP_REFERER']);
-//             exit();
-//         }
-//     }
-// }
->>>>>>> d2386b70e125ec50377039c94b6b4740677d5870
-
 
     public function update()
     {
@@ -103,7 +67,6 @@ class CartController extends Cart
                 exit();
             }
     }
-    
 }
 public function delete()
     {
